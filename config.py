b3fe9a2fe2f9cdcafa8953a831e98a98c797286a
avantikaa02 <avantikaa2470004@ssn.edu.in>
2026-01-03 18:16:45 +0530

    A added initial backend files

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+VISA_SANDBOX_BASE_URL = "https://sandbox.api.visa.com"
+
+VISA_API_KEY = "DUMMY_API_KEY"
+VISA_SHARED_SECRET = "DUMMY_SECRET"
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
